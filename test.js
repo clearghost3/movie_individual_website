9f15697968ee0df01b0f9842e5be8641a8fcf453
clearghost3 <165910897+clearghost3@users.noreply.github.com>
2024-05-06 18:10:43 +0900

    start file

---
+++ test.js
@@ -0,0 +1,12 @@
+const Pwd="hello.@yunh";
+const Name="김철수"
+const Rating="5점"
+const Comments="정말 재미있었어요"
+const Review = {
+    pwd:Pwd,
+    "name":Name,
+    "rating":Rating,
+    "comments":Comments,
+}
+
+console.log(Review["pwd"]);
